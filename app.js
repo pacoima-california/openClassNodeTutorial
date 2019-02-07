1e7a21a73bffa8a85ea9bdb6f95688598c4d6624
Hector Herrera <hector.rogelio.herrera@gmail.com>
2019-02-07 15:51:51 -0600

    playing with node server. used req(url) and req(http). the url part seems to have functions that can parse out the info from req. html has functions that allow for server functions

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
+
+
+var url = require("url");
+
 var http = require('http');
 function serverCreation(req, res){
-  res.writeHead(200);
-  res.end("hello world");
+  var page = url.parse(req.url).pathname;
+  console.log(page); 
+
+  res.writeHead(200, {"Content-Type": "text/html"});
+  res.write("hello world");
+  res.end();
 }
 var server = http.createServer(serverCreation);
+
 
 server.listen(8080);
